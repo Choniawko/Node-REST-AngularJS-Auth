cc479c06c90f2d0749c2e8353076f55d872cad2f
choniawko <pawel.choniawko@gmail.com>
2016-01-20 22:27:41 +0100

    jsonwebtoken

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var express = require('express');
-var faker   = require('faker');
-var cors    = require('cors');
+var express    = require('express');
+var faker      = require('faker');
+var cors       = require('cors');
 var bodyParser = require('body-parser');
+var jwt        = require('jsonwebtoken'); 
+
+
+var jwtSecret = 'fjkdlsajfoew239053/3uk';
 
 var user = {
 	username: 'admin',
@@ -21,13 +25,26 @@ app.get('/random-user', function(req, res) {
 });
 
 app.post('/login', authenticate, function(req, res) {
-
-})
+	var token = jwt.sign({
+		username: user.username
+	}, jwtSecret);
+	res.send({
+		token: token,
+		user: user
+	});
+});
 
 app.listen(3000, function () {
 	console.log('Aplikacja nasłuchuje na porcie 3000');
 });
 
 function authenticate(req, res, next) {
-
+	var body = req.body;
+	if (!body.username || !body.password) {
+		res.status(400).end('Musisz wprowadzić nazwę użytkownika i hasło');
+	}
+	if (body.username !== user.username || body.password !== user.password) {
+		res.satus(401).end('Nazwa użytkownika lub hasło st nieprawidłowe');
+	}
+	next();
 }
